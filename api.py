b5fb132ea154f09c488aa4327b268cf98b06c532
Robelox22 <robellfernando@gmail.com>
2024-12-11 23:49:28 -0600

    SECOND commit

--- api.py
+++ api.py
@@ -6,6 +6,7 @@ import os
 import uuid
 import orm.repo as repo #funciones para hacer consultas a la BD
 from sqlalchemy.orm import Session
+import orm.esquemas as esquemas
 from orm.config import generador_sesion #generador de sesiones
 
 # creación del servidor
@@ -73,9 +74,56 @@ def fotos_por_id_usr(id:int,sesion:Session=Depends(generador_sesion)):
     print("api consultando todas las fotos")
     return repo.fotos_por_id_alumno(sesion,id)
 
+@app.post("/alumnos")
+def guardar_alumno(alumno:AlumnoBase, parametro1:str):
+    print("alumno a guardar:", alumno, ", parametro1:", parametro1)
+    #simulamos guardado en la base.
+    
+    usr_nuevo = {}
+    usr_nuevo["id"] = len(alumnos)
+    usr_nuevo["nombre"] = alumno.nombre
+    usr_nuevo["edad"] = alumno.edad
+    usr_nuevo["domicilio"] = alumno.domicilio
+
+    alumnos.append(alumno)
+
+    return usr_nuevo
+#----
+
+@app.post("/alumnos")
+def guardar_alumno(alumno:esquemas.AlumnoBase,sesion:Session=Depends(generador_sesion)):
+    print(alumno)
+    #guardado en la base.
+    return repo.guardar_Alumno(sesion,alumno)
+
+@app.post("/alumnos/{id}/fotos")
+def guardar_alumno(alumno:AlumnoBase, parametro1:str):
+    print("alumno a guardar:", alumno, ", parametro1:", parametro1)
+    #simulamos guardado en la base.
+    
+    usr_nuevo = {}
+    usr_nuevo["id"] = len(alumnos)
+    usr_nuevo["nombre"] = alumno.nombre
+    usr_nuevo["edad"] = alumno.edad
+    usr_nuevo["domicilio"] = alumno.domicilio
+
+    alumnos.append(alumno)
+
+    return usr_nuevo
 
 
 
+@app.put("/alumno/{id}")
+def actualiza_alumno(id:int,info_alumno:esquemas.AlumnoBase,sesion:Session=Depends(generador_sesion)):
+    return repo.actualiza_alumno(sesion,id,info_alumno)
+
+
+@app.delete("/alumno/{id}")
+def borrar_alumno(id:int,sesion:Session=Depends(generador_sesion)):
+    repo.borra_alumno_por_id(sesion,id)
+    return {"alumno_borrado", "ok"}
+
+ 
 
 #--------------------calificaciones---------------------------
 
@@ -90,13 +138,6 @@ def foto_por_id(id:int,sesion:Session=Depends(generador_sesion)):
     print("Api consultando fotos por id")
     return repo.foto_por_id(sesion, id)
 
-#----------Fotos----------------------------------------
-
-@app.delete("/fotos/{id}")
-def borrar_foto(id:int,sesion:Session=Depends(generador_sesion)):
-    repo.borra_fotos_por_id(sesion,id)
-    return {"alumno_borrado", "ok"}
-
 @app.delete("/calificaciones/{id}")
 def borrar_calificaciones(id:int,sesion:Session=Depends(generador_sesion)):
     repo.borra_calificaciones_por_id (sesion,id)
@@ -107,14 +148,27 @@ def borrar_alumno(id:int,sesion:Session=Depends(generador_sesion)):
     repo.borra_alumno_por_id_calif(sesion,id)
     return {"alumno_borrado", "ok"}
 
+
+@app.put("/calificaciones/{id}")
+def actualiza_calificacion(id:int,info_alumno:esquemas.CalificacionBase,sesion:Session=Depends(generador_sesion)):
+    return repo.actualiza_calificacion(sesion,id,info_alumno)
+#----------Fotos----------------------------------------
+
+@app.delete("/fotos/{id}")
+def borrar_foto(id:int,sesion:Session=Depends(generador_sesion)):
+    repo.borra_fotos_por_id(sesion,id)
+    return {"alumno_borrado", "ok"}
+
+
+
 @app.delete("/alumno/{id}/fotos")
 def borrar_alumno(id:int,sesion:Session=Depends(generador_sesion)):
     repo.borra_alumno_por_id_foto(sesion,id)
     return {"alumno_borrado", "ok"}
 
-@app.delete("/alumno/{id}")
-def borrar_alumno(id:int,sesion:Session=Depends(generador_sesion)):
-    repo.borra_alumno_por_id(sesion,id)
-    return {"alumno_borrado", "ok"}
+@app.put("/fotos/{id}")
+def actualiza_foto(id:int,info_alumno:esquemas.FotoBase,sesion:Session=Depends(generador_sesion)):
+    return repo.actualiza_foto(sesion,id,info_alumno)
+
 
 
--- orm/repo.py
+++ orm/repo.py
@@ -1,5 +1,6 @@
 import orm.modelos as modelos
 from sqlalchemy.orm import Session
+import orm.esquemas as esquemas
 from sqlalchemy import and_
 
 
@@ -109,3 +110,120 @@ def devuelve_fotos(session:Session):
     print("select * from app.fotos")
     return session.query(modelos.Foto).all()
 
+#POST '/alumnos'
+def guardar_Alumno(sesion:Session, usr_nuevo:esquemas.AlumnoBase):
+    #1.- Crear un nuevo objeto de la clase modelo alumno
+    usr_bd = modelos.Alumno()
+    #2.- Llenamos el nuevo objeto con los parámetros que nos paso el alumno
+    usr_bd.nombre = usr_nuevo.nombre
+    usr_bd.edad = usr_nuevo.edad
+    usr_bd.domicilio = usr_nuevo.domicilio
+    usr_bd.carrera =usr_nuevo.carrera
+    usr_bd.trimestre =usr_nuevo.trimestre
+    usr_bd.email = usr_nuevo.email
+    usr_bd.password = usr_nuevo.password
+    #3.- Insertar el nuevo objeto a la BD
+    sesion.add(usr_bd)
+    #4.- Confirmamos el cambio
+    sesion.commit()
+    #5.- Hacemos un refresh
+    sesion.refresh(usr_bd)
+    return usr_bd
+
+#POST '/calificacion'
+def guardar_calificiacion(sesion:Session, usr_nuevo:esquemas.CalificacionBase):
+    #1.- Crear un nuevo objeto de la clase modelo alumno
+    usr_bd = modelos.Calificacion()
+    #2.- Llenamos el nuevo objeto con los parámetros que nos paso el alumno
+    usr_bd.id_alumno = usr_nuevo.id_alumno
+    usr_bd.uea = usr_nuevo.uea
+    usr_bd.calificacion = usr_nuevo.calificacion
+   
+    #3.- Insertar el nuevo objeto a la BD
+    sesion.add(usr_bd)
+    #4.- Confirmamos el cambio
+    sesion.commit()
+    #5.- Hacemos un refresh
+    sesion.refresh(usr_bd)
+    return usr_bd
+#POST '/calificacion'
+
+def guardar_fotos(sesion:Session, usr_nuevo:esquemas.FotoBase):
+    #1.- Crear un nuevo objeto de la clase modelo alumno
+    usr_bd = modelos.Foto()
+    #2.- Llenamos el nuevo objeto con los parámetros que nos paso el alumno
+    usr_bd.id_alumno = usr_nuevo.id_alumno
+    usr_bd.titulo = usr_nuevo.titulo
+    usr_bd.descripcion= usr_nuevo.descripcion
+    usr_bd.ruta = usr_nuevo.ruta
+   
+    #3.- Insertar el nuevo objeto a la BD
+    sesion.add(usr_bd)
+    #4.- Confirmamos el cambio
+    sesion.commit()
+    #5.- Hacemos un refresh
+    sesion.refresh(usr_bd)
+    return usr_bd
+
+
+def actualiza_alumno(sesion:Session,id_alumno:int,usr_esquema:esquemas.AlumnoBase):
+    #1.-Verificar que el alumno existe
+    usr_bd = alumno_por_id(sesion,id_alumno)
+    if usr_bd is not None:
+        #2.- Actualizamos los datos del usuaurio en la BD
+        usr_bd.nombre = usr_esquema.nombre
+        usr_bd.edad = usr_esquema.edad
+        usr_bd.domicilio = usr_esquema.domicilio
+        usr_bd.carrera =usr_esquema.carrera
+        usr_bd.trimestre =usr_esquema.trimestre
+        usr_bd.email = usr_esquema.email
+        usr_bd.password = usr_esquema.password
+        #3.-Confirmamos los cambios
+        sesion.commit()
+        #4.-Refrescar la BD
+        sesion.refresh(usr_bd)
+        #5.-Imprimir los datos nuevos
+        print(usr_esquema)
+        return usr_esquema
+    else:
+        respuesta = {"mensaje":"No existe el alumno"}
+        return respuesta
+    
+def actualiza_calificacion(sesion:Session,id_alumno:int,usr_esquema:esquemas.CalificacionBase):
+    #1.-Verificar que el alumno existe
+    usr_bd = alumno_por_id(sesion,id_alumno)
+    if usr_bd is not None:
+        #2.- Actualizamos los datos del usuaurio en la BD
+        usr_bd.id_alumno= usr_esquema.id_alumno
+        usr_bd.uea = usr_esquema.uea
+        usr_bd.calificacion = usr_esquema.calificacion  
+        #3.-Confirmamos los cambios
+        sesion.commit()
+        #4.-Refrescar la BD
+        sesion.refresh(usr_bd)
+        #5.-Imprimir los datos nuevos
+        print(usr_esquema)
+        return usr_esquema
+    else:
+        respuesta = {"mensaje":"No existe el alumno"}
+        return respuesta
+    
+def actualiza_foto(sesion:Session,id_alumno:int,usr_esquema:esquemas.FotoBase):
+    #1.-Verificar que el alumno existe
+    usr_bd = alumno_por_id(sesion,id_alumno)
+    if usr_bd is not None:
+        #2.- Actualizamos los datos del usuaurio en la BD
+        usr_bd.id_alumno= usr_esquema.id_alumno
+        usr_bd.uea = usr_esquema.titulo
+        usr_bd.descripcion= usr_esquema.descripcion
+        usr_bd.ruta = usr_esquema.ruta
+        #3.-Confirmamos los cambios
+        sesion.commit()
+        #4.-Refrescar la BD
+        sesion.refresh(usr_bd)
+        #5.-Imprimir los datos nuevos
+        print(usr_esquema)
+        return usr_esquema
+    else:
+        respuesta = {"mensaje":"No existe el alumno"}
+        return respuesta
